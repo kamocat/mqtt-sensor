2a20b82e47dee27a76f06a55f0d9123b3fffbc2b
Marshal Horn <kamocat@gmail.com>
2022-04-21 17:01:50 -0700

    Interrupt isn't working right

--- main/sonar.cpp
+++ main/sonar.cpp
@@ -9,54 +9,66 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "driver/gpio.h"
-#include "sdkconfig.h"
 #include "esp_timer.h"
 
-/* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-const gpio_num_t BLINK_GPIO = gpio_num_t(CONFIG_BLINK_GPIO);
+static xQueueHandle sonar = NULL;
+
+int64_t time;
+static void sonar_pulse_handler(void * arg){
+    time = esp_timer_get_time();
+    xQueueSendFromISR(sonar, &time, NULL);
+}
+
 const gpio_num_t TRIG = gpio_num_t(22);
 const gpio_num_t ECHO = gpio_num_t(23);
 
-uint32_t sonar_pulse(void){
+int16_t sonar_pulse(void){
+    
+    if(gpio_get_level(ECHO))
+        return -3; // previous pulse did not finish
     //Send pulse
     gpio_set_level(TRIG, 0);
     vTaskDelay(0.01 / portTICK_PERIOD_MS);
     gpio_set_level(TRIG, 1);
 
-    // 2ms delay before echo begins
-    vTaskDelay(2 / portTICK_PERIOD_MS);
-
     //Wait for echo
-    while(!gpio_get_level(ECHO));
-    int64_t start = esp_timer_get_time();
-    while(gpio_get_level(ECHO));
-    int64_t stop = esp_timer_get_time();
+    int64_t start, stop;
+    if(!xQueueReceive(sonar, &start, 3 / portTICK_PERIOD_MS))
+        return -1;
+    if(!xQueueReceive(sonar, &stop, 70 / portTICK_PERIOD_MS))
+        return -2; // Object is too close in front of sonar
 
-
-    return (stop-start)*0.1715; // millimeters per microsecond
+    double result = (stop-start)*0.1715; // millimeters per microsecond
+    if(result > 10e3)
+        return 10e3;
+    else if(result < 0)
+        return -4;
+    return result;
 }
 
 
 
 extern "C" void app_main(void)
 {
-    /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
-       muxed to GPIO on reset already, but some default to other
+    /* Configure the IOMUX register (some pads are muxed to GPIO 
+	on reset already, but some default to other
        functions and need to be switched to GPIO. Consult the
        Technical Reference for a list of pads and their default
        functions.)
     */
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    sonar = xQueueCreate( 10, sizeof(int64_t));
+    gpio_reset_pin(TRIG);
+    gpio_reset_pin(ECHO);
     gpio_set_direction(TRIG, GPIO_MODE_OUTPUT);
     gpio_set_direction(ECHO, GPIO_MODE_INPUT);
+    gpio_set_intr_type(ECHO, GPIO_INTR_ANYEDGE);
+    gpio_install_isr_service(ESP_INTR_FLAG_LEVEL1); // High priority interrupt
+    gpio_isr_handler_add(ECHO, sonar_pulse_handler, (void*) NULL);
     gpio_set_level(TRIG, 1);
     while(1) {
-        uint32_t dist = sonar_pulse();
+        int16_t dist = sonar_pulse();
         printf("Object is %d mm away\n", dist);
         vTaskDelay(100 / portTICK_PERIOD_MS);
 
