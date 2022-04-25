4275e36be31bb7247b465fa9d2ea3f6d2984f452
Marshal Horn <kamocat@gmail.com>
2022-04-24 22:25:23 -0700

    Switched to mcpwm input capture, but interrupt isn't firing

--- main/sonar.cpp
+++ main/sonar.cpp
@@ -12,19 +12,27 @@
 #include "freertos/queue.h"
 #include "driver/gpio.h"
 #include "esp_timer.h"
+#include "soc/rtc.h"
+#include "driver/mcpwm.h"
+#include "soc/mcpwm_periph.h"
 
 static xQueueHandle sonar = NULL;
-
-int64_t time;
-static void sonar_pulse_handler(void * arg){
-    time = esp_timer_get_time();
-    xQueueSendFromISR(sonar, &time, NULL);
-}
-
 const gpio_num_t TRIG = gpio_num_t(22);
 const gpio_num_t ECHO = gpio_num_t(23);
 
-int16_t sonar_pulse(void){
+int32_t rise, fall, diff;
+static void IRAM_ATTR sonar_pulse_handler(void * arg){
+    int32_t tmp = mcpwm_capture_signal_get_value(MCPWM_UNIT_0, MCPWM_SELECT_CAP0); //get capture signal counter value
+    if(mcpwm_capture_signal_get_edge(MCPWM_UNIT_0, MCPWM_SELECT_CAP0)){
+        rise = tmp;
+    } else{
+        fall = tmp;
+    }
+    diff = fall - rise;
+    xQueueSendFromISR(sonar, &diff, NULL);
+}
+
+double sonar_pulse(void){
     
     if(gpio_get_level(ECHO))
         return -3; // previous pulse did not finish
@@ -34,42 +42,44 @@ int16_t sonar_pulse(void){
     gpio_set_level(TRIG, 1);
 
     //Wait for echo
-    int64_t start, stop;
-    if(!xQueueReceive(sonar, &start, 3 / portTICK_PERIOD_MS))
-        return -1;
-    if(!xQueueReceive(sonar, &stop, 70 / portTICK_PERIOD_MS))
+    int32_t pulse;
+    if(!xQueueReceive(sonar, &pulse, 70 / portTICK_PERIOD_MS))
         return -2; // Object is too close in front of sonar
 
-    double result = (stop-start)*0.1715; // millimeters per microsecond
-    if(result > 10e3)
-        return 10e3;
-    else if(result < 0)
-        return -4;
-    return result;
+    double coef = 0.1715; // millimeters per microsecond
+    double f = rtc_clk_apb_freq_get() / 1000000;
+    return pulse * coef / f;
 }
 
-
-
-extern "C" void app_main(void)
+static void example_gpio_init(void)
 {
-    /* Configure the IOMUX register (some pads are muxed to GPIO 
-	on reset already, but some default to other
-       functions and need to be switched to GPIO. Consult the
-       Technical Reference for a list of pads and their default
-       functions.)
-    */
-    sonar = xQueueCreate( 10, sizeof(int64_t));
+    printf("initializing mcpwm gpio...\n");
     gpio_reset_pin(TRIG);
     gpio_reset_pin(ECHO);
     gpio_set_direction(TRIG, GPIO_MODE_OUTPUT);
     gpio_set_direction(ECHO, GPIO_MODE_INPUT);
-    gpio_set_intr_type(ECHO, GPIO_INTR_ANYEDGE);
-    gpio_install_isr_service(ESP_INTR_FLAG_LEVEL1); // High priority interrupt
-    gpio_isr_handler_add(ECHO, sonar_pulse_handler, (void*) NULL);
-    gpio_set_level(TRIG, 1);
+    gpio_pulldown_en(ECHO);
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM_CAP_0, ECHO);
+}
+
+
+static void capture_config(void)
+{
+    example_gpio_init();
+
+    //In general practice you can connect Capture  to external signal, measure time between rising edge or falling edge and take action accordingly
+    mcpwm_capture_enable(MCPWM_UNIT_0, MCPWM_SELECT_CAP0, MCPWM_BOTH_EDGE, 0);  //capture signal on rising edge, prescale = 0 i.e. capture every edge
+    mcpwm_isr_register(MCPWM_UNIT_0, sonar_pulse_handler, NULL, ESP_INTR_FLAG_IRAM, NULL);  //Set ISR Handler
+}
+
+
+extern "C" void app_main(void)
+{
+    capture_config();
+    sonar = xQueueCreate( 10, sizeof(int64_t));
     while(1) {
-        int16_t dist = sonar_pulse();
-        printf("Object is %d mm away\n", dist);
+        double dist = sonar_pulse();
+        printf("Object is %f mm away\n", dist);
         vTaskDelay(100 / portTICK_PERIOD_MS);
 
     }
